ad1955d64aefc7433da6aec88edf137150da6634
kaviya150 <147969526+kaviya150@users.noreply.github.com>
2023-10-15 12:46:03 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,21 @@
+"""
+
+write a function called linear_search that takes the list of products and a target product name as input.The function should perform a linear searh to find the target product in the list and return a list of indices of all occurrences of the product if founder,or an empty list if the product is not found.
+"""
+def linearSearchProduct(productList,targetproduct):
+  indices = []
+  
+  for index, product in enumerate(productList):
+    if product == targetproduct:
+      indices.append(index)
+
+  return indices
+
+
+
+#Example usage:
+products=["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+target2 = "apple"
+result = linearSearchProduct(products,target)
+print(result)
